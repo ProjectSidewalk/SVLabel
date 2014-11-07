2eaf40dbf2de4754641079aece3f162c236fd79d
amagoon <amagoon17@gmail.com>
2014-11-06 21:30:01 -0500

    ORM for sidewalk.LabelingTasks

--- sidewalk-slick-test/src/main/scala/LabelingTasks.scala
+++ sidewalk-slick-test/src/main/scala/LabelingTasks.scala
@@ -6,12 +6,19 @@ import scala.slick.driver.H2Driver.simple._
 import scala.slick.lifted.ProvenShape
 
 class LabelingTasks(tag: Tag)
-  extends Table[(Int)](tag, "LabelingTasks") {
+  extends Table[(Int,Int,Int,String,Int,String,Int,String)](tag, "LabelingTasks") {
 
-  // Todo: Akash
+  // Akash Magoon 11/6/2014
+  // Object Relational Mapping for sidewalk.LabelingTasks
   def LabelingTaskId: Column[Int] = column[Int]("LabelingTaskId", O.PrimaryKey)
+  def AssignmentId: Column[Int] = column[Int]("AssignmentId")
+  def TaskPanoramaId: Column[Int] = column[Int]("TaskPanoramaId")
+  def TaskGSVPanoramaId: Column[String] = column[String]("TaskGSVPanoramaId")
+  def NoLabel: Column[Int] = column[Int]("NoLabel")
+  def Description: Column[String] = column[String]("Description")
+  def PreviousLabelingTaskId: Column[Int] = column[Int]("PreviousLabelingTaskId")
+  def DatetimeInserted: Column[String] = column[String]("DatetimeInserted")
 
-  // Todo: Akash
-  def * : ProvenShape[(Int)] =
-    (LabelingTaskId)
+  def * : ProvenShape[(Int,Int,Int,String,Int,String,Int,String)] =
+    (LabelingTaskId,AssignmentId,TaskPanoramaId,TaskGSVPanoramaId,NoLabel,Description,PreviousLabelingTaskId,DatetimeInserted)
 }
