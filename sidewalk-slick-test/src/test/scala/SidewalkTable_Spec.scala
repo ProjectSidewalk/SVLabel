2c44a1b97b47c7207c5450b88ec278c8691fc4e8
amagoon <amagoon17@gmail.com>
2014-11-09 05:02:15 -0500

    Updated SidewalkTable_Spec.scala (5/5 tests pass)

--- sidewalk-slick-test/src/test/scala/SidewalkTable_Spec.scala
+++ sidewalk-slick-test/src/test/scala/SidewalkTable_Spec.scala
@@ -4,20 +4,19 @@ import scala.slick.driver.H2Driver.simple._
 // import scala.slick.driver.MySQLDriver.simple._
 import scala.slick.jdbc.meta._
 
-
+// Tests updated by Akash Magoon 11/8/2014
 class SidewalkTable_Spec extends FunSuite with BeforeAndAfter {
   val assignments = TableQuery[Assignments]
-  val labelingTasks = TableQuery[LabelingTasks]
+  val LabelingTasks = TableQuery[LabelingTasks]
 
   implicit var session: Session = _
 
   // Create table
   // Data Definition Language (DDL): http://slick.typesafe.com/doc/2.0.3/schemas.html#data-definition-language
-  def createSchema() = (assignments.ddl ++ labelingTasks.ddl).create // Todo: Akash
+  def createSchema() = (assignments.ddl ++ LabelingTasks.ddl).create // Todo: Akash
 
   def insertAssignment(): Int = assignments += (1,"TestTurkerId","TestHit","TestAssignment","StreetViewLabeler","3", 1,	0,"PilotTask","2013-06-21 18:03:28")
-
-
+  def insertLabelingTasks(): Int = LabelingTasks += (2, 1, 3, "3dlyB8Z0jFmZKSsTQJjMQg", 0, "undefined", 0, "NULL")
   before {
     session = Database.forURL("jdbc:h2:mem:test1", driver = "org.h2.Driver").createSession()
     //session = Database.forURL("jdbc:mysql://localhost:3306/sidewalk-test", driver="com.mysql.jdbc.Driver", user="root", password="").createSession()
@@ -32,6 +31,7 @@ class SidewalkTable_Spec extends FunSuite with BeforeAndAfter {
     assert(tables.count(_.name.name.equalsIgnoreCase("assignments")) == 1)
   }
 
+
   test("Inserting an Assignment works") {
     session.withTransaction {
       createSchema()
@@ -43,9 +43,10 @@ class SidewalkTable_Spec extends FunSuite with BeforeAndAfter {
     }
 
     session.withTransaction {
-      assignments += (1,	"TestTurkerId",	"TestHit",	"TestAssignment",	"StreetViewLabeler",	"3", 1,	0, "PilotTask",	"2013-06-21 18:03:28")
-      assignments += (2,	"Test_Kotaro",	"Test_Hit",	"Test_Assignment",	"StreetViewLabeler",	"3",	1,	0,	"PilotTask",	"2013-06-28 14:19:17")
-      assignments += (3,	"Researcher_Jonah",	"Test_Hit",	"Test_Assignment",	"StreetViewLabeler",	"3",	1,	0,	"ResearcherTask",	"2013-07-03 12:24:36")
+
+      assignments +=(1, "TestTurkerId", "TestHit", "TestAssignment", "StreetViewLabeler", "3", 1, 0, "PilotTask", "2013-06-21 18:03:28")
+      assignments +=(2, "Test_Kotaro", "Test_Hit", "Test_Assignment", "StreetViewLabeler", "3", 1, 0, "PilotTask", "2013-06-28 14:19:17")
+      assignments +=(3, "Researcher_Jonah", "Test_Hit", "Test_Assignment", "StreetViewLabeler", "3", 1, 0, "ResearcherTask", "2013-07-03 12:24:36")
 
       val results = assignments.list
       assert(results.size == 3)
@@ -56,6 +57,7 @@ class SidewalkTable_Spec extends FunSuite with BeforeAndAfter {
       session.rollback()
     }
   }
+
 
   test("Query Assignments works") {
     session.withTransaction {
@@ -72,47 +74,57 @@ class SidewalkTable_Spec extends FunSuite with BeforeAndAfter {
   // Todo: Akash
   test("Inserting LabelingTasks works") {
     session.withTransaction {
-      labelingTasks += (2, 1, 3, "3dlyB8Z0jFmZKSsTQJjMQg",	0, "undefined", 0, "undefined")
-      labelingTasks += (3,	2, 3,	"3dlyB8Z0jFmZKSsTQJjMQg",	0, "undefined", 0, "undefined")
-      labelingTasks += (4, 2, 4, "_AUz5cV_ofocoDbesxY3Kw", 0, "undefined",	0, "undefined")
+      createSchema()
 
-      val results = labelingTasks.list
+      val insertCount = insertLabelingTasks()
+      assert(insertCount == 1)
+
+      session.rollback()
+    }
+    session.withTransaction {
+      LabelingTasks += (2, 1, 3, "3dlyB8Z0jFmZKSsTQJjMQg",	0, "undefined", 0, "undefined")
+      LabelingTasks += (3,	2, 3,	"3dlyB8Z0jFmZKSsTQJjMQg",	0, "undefined", 0, "undefined")
+      LabelingTasks += (4, 2, 4, "_AUz5cV_ofocoDbesxY3Kw", 0, "undefined",	0, "undefined")
+
+      val results = LabelingTasks.list
       assert(results.size == 3)
       assert(results.head._1 == 2)
     }
   }
 
-  test("Inserting LabelingTasks works") {
+  // Inner join test, joining assignments and LabelingTasks where AssignmentIds for both are equal
+  test("Inner join test: assignments and LabelingTasks") {
+
     session.withTransaction {
+
+      createSchema()
+
       assignments += (1,	"TestTurkerId",	"TestHit",	"TestAssignment",	"StreetViewLabeler",	"3", 1,	0, "PilotTask",	"2013-06-21 18:03:28")
       assignments += (2,	"Test_Kotaro",	"Test_Hit",	"Test_Assignment",	"StreetViewLabeler",	"3",	1,	0,	"PilotTask",	"2013-06-28 14:19:17")
       assignments += (3,	"Researcher_Jonah",	"Test_Hit",	"Test_Assignment",	"StreetViewLabeler",	"3",	1,	0,	"ResearcherTask",	"2013-07-03 12:24:36")
 
-      labelingTasks += (2, 1, 3, "3dlyB8Z0jFmZKSsTQJjMQg",	0, "undefined", 0, "undefined")
-      labelingTasks += (3, 2, 3,	"3dlyB8Z0jFmZKSsTQJjMQg",	0, "undefined", 0, "undefined")
-      labelingTasks += (4, 2, 4, "_AUz5cV_ofocoDbesxY3Kw", 0, "undefined",	0, "undefined")
+      LabelingTasks += (2, 1, 3, "3dlyB8Z0jFmZKSsTQJjMQg",	0, "undefined", 0, "undefined")
+      LabelingTasks += (3, 2, 3,	"3dlyB8Z0jFmZKSsTQJjMQg",	0, "undefined", 0, "undefined")
+      LabelingTasks += (4, 2, 4, "_AUz5cV_ofocoDbesxY3Kw", 0, "undefined",	0, "undefined")
 
-      // Slick inner join
-      // http://slick.typesafe.com/doc/2.1.0/queries.html
-      val joined = for {
-        (a, l) <- assignments innerJoin labelingTasks on (_.AssignmentId === _.AssignmentId)
-      } yield (a.AssignmentId, a.AmazonTurkerId, l.LabelingTaskId)
+     //Slick inner join
+     //http://slick.typesafe.com/doc/2.1.0/queries.html
+     /*val joined = for {
+        (a, l) <- assignments innerJoin LabelingTasks on (_.AssignmentId === _.AssignmentId)
+      } yield (a.AssignmentId, a.AmazonTurkerId, l.LabelingTaskId)*/
 
-      val results = joined.list
+      val innerjoin = for {
+        a <- assignments
+        l <- LabelingTasks if a.AssignmentId === l.AssignmentId
+      } yield(a.AssignmentId, a.AmazonTurkerId, l.LabelingTaskId)
 
-      assert(results.size == 2)
+      val results = innerjoin.list
 
-      // Ones that will remain with inner join should be the ones with AssignmentId == 2.
-      assert(results.head._1 == 2)
+      assert(results.size == 3)
+      assert(results.head._1 == 1)
+      assert(results.head._2 == "TestTurkerId")
 
-      // The TurkerId should be "Test_Kotaro"
-      assert(results.head._2 == "Test_Kotaro")
-
-      // The Google Street View id should be '3dlyB8Z0jFmZKSsTQJjMQg'
-      assert(results.head._3 == "3dlyB8Z0jFmZKSsTQJjMQg")
-
-    }
-  }
+  }}
 
 
   after {
