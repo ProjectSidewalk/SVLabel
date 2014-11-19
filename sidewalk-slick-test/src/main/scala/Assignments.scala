4d6e6e70339779ec8787f963cca0068167440e77
Kotaro Hara <koe.bluebear@gmail.com>
2014-11-19 17:06:36 -0500

    Started using DateTime instead of String
    This allows us to sort the data if needed. And it makes more sense.
    Additionally, the Assignment model takes a case class instead of a
    tuple for a parameter

--- sidewalk-slick-test/src/main/scala/Assignments.scala
+++ sidewalk-slick-test/src/main/scala/Assignments.scala
@@ -4,9 +4,15 @@
 import scala.slick.driver.H2Driver.simple._
 //import scala.slick.driver.MySQLDriver.simple._
 import scala.slick.lifted.ProvenShape
+import java.sql.Timestamp
+import org.joda.time.DateTime
+
+case class Assignment(AssignmentId: Int, AmazonTurkerId: String, AmazonHitId: String, AmazonAssignmentId: String,
+                      InterfaceType: String, InterfaceVersion: String, Completed: Int, NeedQualification: Int,
+                      TaskDescription: String, DatetimeInserted: DateTime)
 
 class Assignments(tag: Tag)
-  extends Table[(Int, String, String, String, String, String, Int, Int, String, String)](tag, "Assignments") {
+  extends Table[Assignment](tag, "Assignments") {
 
   // This is the primary key column:
   def AssignmentId: Column[Int] = column[Int]("AssignmentId", O.PrimaryKey)
@@ -18,10 +24,23 @@ class Assignments(tag: Tag)
   def Completed: Column[Int] = column[Int]("Completed")
   def NeedQualification: Column[Int] = column[Int]("NeedQualification")
   def TaskDescription: Column[String] = column[String]("TaskDescription")
-  def DatetimeInserted: Column[String] = column[String]("DatetimeInserted")
 
-  // Every table needs a * projection with the same type as the table's type parameter
-  def * : ProvenShape[(Int, String, String, String, String, String, Int, Int, String, String)] =
-    (AssignmentId, AmazonTurkerId, AmazonHitId, AmazonAssignmentId, InterfaceType, InterfaceVersion, Completed,
-      NeedQualification, TaskDescription, DatetimeInserted)
+  // Converting joda datetime
+  // http://stackoverflow.com/questions/22942202/slick-library-w-play-date-type-incompatibility
+  // https://groups.google.com/forum/#!msg/scalaquery/4Ns_J_8wbqQ/0SGiJL4O8A8J
+  // http://www.epochconverter.com/
+  implicit val dateTimeColumnType = MappedColumnType.base[DateTime, Timestamp](
+    { dt => new java.sql.Timestamp(dt.getMillis) },
+    { ts => new DateTime(ts) }
+  )
+
+  def DatetimeInserted: Column[DateTime] = column[DateTime]("DatetimeInserted")
+
+  def * = (AssignmentId, AmazonTurkerId, AmazonHitId, AmazonAssignmentId, InterfaceType, InterfaceVersion, Completed,
+    NeedQualification, TaskDescription, DatetimeInserted) <> (Assignment.tupled, Assignment.unapply _)
 }
+  // Every table needs a * projection with the same type as the table's type parameter
+//  def * : ProvenShape[(Int, String, String, String, String, String, Int, Int, String, String)] =
+//    (AssignmentId, AmazonTurkerId, AmazonHitId, AmazonAssignmentId, InterfaceType, InterfaceVersion, Completed,
+//      NeedQualification, TaskDescription, DatetimeInserted)
+//}
