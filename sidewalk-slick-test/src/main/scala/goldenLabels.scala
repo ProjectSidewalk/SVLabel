8f8c629e5f4fd4be226ba771351a019f485733c8
Kotaro Hara <koe.bluebear@gmail.com>
2014-11-19 20:02:05 -0500

    changed GoldenLabels

--- sidewalk-slick-test/src/main/scala/goldenLabels.scala
+++ sidewalk-slick-test/src/main/scala/goldenLabels.scala
@@ -1,17 +1,16 @@
-/**
- * Created by Akash on 11/19/2014.
- */
 import scala.slick.driver.H2Driver.simple._
 import scala.slick.lifted.ProvenShape
-class goldenLabels(tag: Tag)
-  extends Table[(Int,Int,Int)](tag, "goldenLabels") {
+
+case class GoldenLabel(GoldenLabelId: Int, TaskImageId: Int, LabelTypeId: Int)
+
+class GoldenLabels(tag: Tag)
+  extends Table[GoldenLabel](tag, "goldenLabels") {
 
   def GoldenLabelId: Column[Int] = column[Int]("GoldenLabelId", O.PrimaryKey)
   def TaskImageId: Column[Int] = column[Int]("TaskImageId")
   def LabelTypeId: Column[Int] = column[Int]("LabelTypeId")
 
-
-  def * : ProvenShape[(Int,Int,Int)] =
-    (GoldenLabelId,TaskImageId,LabelTypeId)
-
+//  def * : ProvenShape[(Int,Int,Int)] =
+//    (GoldenLabelId,TaskImageId,LabelTypeId)
+  def * = (GoldenLabelId, TaskImageId, LabelTypeId) <> (GoldenLabel.tupled, GoldenLabel.unapply _)
 }
