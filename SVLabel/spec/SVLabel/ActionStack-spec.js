9ec1526cdfec3fb41dba25c3e174b883a5f9ece1
Kotaro Hara <koe.bluebear@gmail.com>
2015-02-20 16:35:39 -0500

    Added ActionStack-spec and Canvas-spec

---
+++ SVLabel/spec/SVLabel/ActionStack-spec.js
@@ -0,0 +1,23 @@
+describe("The ActionStack module's basic API", function () {
+  var stack = new ActionStack({});
+
+
+  describe("Test pop", function () {
+  });
+
+  describe("Test push", function () {
+
+  });
+
+  describe("Test redo", function () {
+
+  });
+
+  describe("Test undo", function () {
+
+  });
+
+  describe("Test redo", function () {
+
+  });
+});
